e1855f64044bc79da9eaf5c3e1f6a9fd7e9d1aec
mveyone <mveyone@outlook.fr>
2023-06-23 01:12:36 +0100

    eks

--- routing-tables.tf
+++ routing-tables.tf
@@ -2,19 +2,19 @@ resource "aws_route_table" "public" {
 
   vpc_id = aws_vpc.main.id
 
-  route  {
+  route {
 
     #the cidr block of the route 
     cidr_block = "0.0.0.0/0"
 
     gateway_id = aws_internet_gateway.main.id
 
-    
+
 
   }
   tags = {
-      Name = "public"
-    }
+    Name = "public"
+  }
 
 }
 
@@ -22,18 +22,18 @@ resource "aws_route_table" "private1" {
 
   vpc_id = aws_vpc.main.id
 
-  route  {
+  route {
 
     #the cidr block of the route 
     cidr_block = "0.0.0.0/0"
 
     nat_gateway_id = aws_nat_gateway.gw1.id
 
-   
+
   }
- tags = {
-      Name = "private1"
-    }
+  tags = {
+    Name = "private1"
+  }
 
 }
 
@@ -49,8 +49,8 @@ resource "aws_route_table" "private2" {
     nat_gateway_id = aws_nat_gateway.gw2.id
 
   }
-     tags = {
-      Name = "private2"
-    }
+  tags = {
+    Name = "private2"
+  }
 
 }
